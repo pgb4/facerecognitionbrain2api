473a2e5e661e56349fe935386685acc1251066bc
pgb4 <preston.bebas1@gmail.com>
2019-02-13 12:26:09 -0500

    update db ref

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const profile = require('./controllers/profile')
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '4Postgres45??',
-    database : 'smart_brain2'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
